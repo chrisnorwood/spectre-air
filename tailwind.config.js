191a712c0fef0f98ab26468a3127b2d6effa0369
Chris Norwood <cnorwood92@gmail.com>
2019-07-16 11:36:53 -0700

    Change tailwind config name, and include some theme colors

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'orange': '#fb8b24',
+        'darkred': '#f4442e',
+        'darkgrey': '#4d4d4d',
+      },
+    },
+  },
+  variants: {},
+  plugins: []
+}
