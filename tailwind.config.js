f701b233e4897d683c7892d8ccfc6308dad98020
Chris Norwood <cnorwood92@gmail.com>
2019-07-29 17:17:46 -0700

    Add more inner content

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'orange': '#fb8b24',
+        'myorange': '#fb8b24',
         'darkred': '#f4442e',
         'darkgrey': '#4d4d4d',
       },
